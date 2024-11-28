8ddafeff45518cf391d80fa95c9c5eb86d49e6a6
Nicolas Lopes <nicolas@clerk.dev>
2024-11-28 13:06:14 -0300

    nicolas patch

--- samlsp/fetch_metadata.go
+++ samlsp/fetch_metadata.go
@@ -12,9 +12,9 @@ import (
 	"github.com/crewjam/httperr"
 	xrv "github.com/mattermost/xml-roundtrip-validator"
 
-	"github.com/crewjam/saml/logger"
+	"github.com/clerk/saml/logger"
 
-	"github.com/crewjam/saml"
+	"github.com/clerk/saml"
 )
 
 // ParseMetadata parses arbitrary SAML IDP metadata.
