e5ceab460c54de55856e351ef644e422bcab50ba
Nicolas Lopes <nicolas@clerk.dev>
2024-11-28 12:40:00 -0300

    Revert "Fix IDP deadlock due to recursive locking"
    This reverts commit 91716e8856a3a1723129577d6dfb5352d06fc837.

--- samlidp/samlidp.go
+++ samlidp/samlidp.go
@@ -100,6 +100,8 @@ func (s *Server) InitializeHTTP() {
 		s.IDP.ServeMetadata(w, r)
 	})
 	mux.HandleFunc("/sso", func(w http.ResponseWriter, r *http.Request) {
+		s.idpConfigMu.RLock()
+		defer s.idpConfigMu.RUnlock()
 		s.IDP.ServeSSO(w, r)
 	})
 
